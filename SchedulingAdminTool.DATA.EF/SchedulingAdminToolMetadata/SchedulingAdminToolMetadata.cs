6693e63366e9a7b3ef861ef98f777b290e951766
Jwoodworth515 <84031813+Jwoodworth515@users.noreply.github.com>
2021-08-04 10:36:38 -0500

    BuildIssueCommit

--- SchedulingAdminTool.DATA.EF/SchedulingAdminToolMetadata/SchedulingAdminToolMetadata.cs
+++ SchedulingAdminTool.DATA.EF/SchedulingAdminToolMetadata/SchedulingAdminToolMetadata.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchedulingAdminTool.DATA.EF
+namespace SchedulingAdminTool.DATA.EF/*.SchedulingAdminToolMetadata*/
 {
-    public class Class1
+    class SchedulingAdminToolMetadata
     {
     }
 }
