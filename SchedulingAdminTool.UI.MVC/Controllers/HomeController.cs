6fc230aa2f984475d8e41555e38f9d52ee057de1
Jwoodworth515 <84031813+Jwoodworth515@users.noreply.github.com>
2021-08-05 11:12:08 -0500

    ContactFormCommit

--- SchedulingAdminTool.UI.MVC/Controllers/HomeController.cs
+++ SchedulingAdminTool.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
 ﻿using System.Web.Mvc;
+using SchedulingAdminTool.UI.MVC.Models;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
 
 namespace SchedulingAdminTool.UI.MVC.Controllers
 {
@@ -11,7 +16,6 @@ namespace SchedulingAdminTool.UI.MVC.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
@@ -25,6 +29,42 @@ namespace SchedulingAdminTool.UI.MVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/> {cvm.Message}";
+
+            MailMessage mm = new MailMessage(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailTo"].ToString(), cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+            client.Port = 8889; //for gmail
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"Friends Don't Lie! Something went wrong and we couldn't process your request. Please try again later. Error Message: </br> {ex.StackTrace}";
+                return View(cvm);
+            }
+            return View("EmailConfirmation", cvm);
+
+
         }
     }
 }
---
+++ SchedulingAdminTool.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchedulingAdminTool.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Friends Don't Lie! Name is required")]        public string Name { get; set; }        [Required(ErrorMessage = "Friends Don't Lie! Email is required")]        [DataType(DataType.EmailAddress)] //EmailAddress is an option that pops up in intellisense
+        public string Email { get; set; }        [Required(ErrorMessage = "Friends Don't Lie! Subject is required")]        public string Subject { get; set; }        [Required(ErrorMessage = "Friends Don't Lie! Message is required")]        [UIHint("MultilineText")]        public string Message { get; set; }
+
+
+    }
+}
